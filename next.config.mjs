402579111e58994d3b12ac721626320d840bcb06
spadev <spadev2015@gmail.com>
2024-06-07 13:51:55 -0700

    project launch to vercel
    project to vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'pbs.twimg.com',
+          port: '',
+          pathname: '/profile_images/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'cloudflare-ipfs.com',
+          port: '',
+          pathname: '/ipfs/**',
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
